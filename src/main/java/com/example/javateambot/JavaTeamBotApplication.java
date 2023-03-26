0f17e47b10356dd25d7f4bddeebc1c2f95845088
Kolomnin <kolomninsp@gmail.com>
2023-03-26 21:08:16 +0300

    First commit

---
+++ src/main/java/com/example/javateambot/JavaTeamBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.javateambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTeamBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTeamBotApplication.class, args);
+    }
+
+}
