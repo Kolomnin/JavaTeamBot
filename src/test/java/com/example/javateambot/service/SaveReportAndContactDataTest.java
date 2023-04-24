41e207f59b1d7583c44d8486f5fa13ac6ff6d425
Sergey <seregapanin23703@gmail.com>
2023-04-24 20:15:41 +0300

    Вынес в отдельный класс методы сохранения, удалил лишний текст

---
+++ src/test/java/com/example/javateambot/service/SaveReportAndContactDataTest.java
@@ -0,0 +1,16 @@
+package com.example.javateambot.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaveReportAndContactDataTest {
+
+    @Test
+    void saveReport() {
+    }
+
+    @Test
+    void saveContactData() {
+    }
+}
